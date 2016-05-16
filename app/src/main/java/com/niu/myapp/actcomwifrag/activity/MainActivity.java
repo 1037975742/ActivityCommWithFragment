d018e98aa698041ef76617e7b508e10a3435464c
wangdenghui <wangdenghui351@pingan.com.cn>
2016-05-16 13:32:28 +0800

    修改MainActivity

--- app/src/main/java/com/niu/myapp/actcomwifrag/activity/MainActivity.java
+++ app/src/main/java/com/niu/myapp/actcomwifrag/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package com.niu.myapp.actcomwifrag.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.widget.Toast;
